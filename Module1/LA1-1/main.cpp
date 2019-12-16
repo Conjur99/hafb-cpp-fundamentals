c606784f6d67f8e54c5a93064304cdeaa97cb294
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 09:53:46 -0700

    first commit of main

---
+++ Module1/LA1-1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;    //int: signed integers
+
+    //cout: for system output
+    std::cout << "Now in github" <<std::endl;
+    cout << "Another line" <<endl;
+    cout << "Nittany Lions "<<"PSU "<<"Penna"<<endl;
+   
+    //cin: for system/user input
+    cout << "Enter your age?" << endl;
+    cin >> age;
+    cout << "I see you are: " << age << endl;
+
+    return 0;
+
+}
