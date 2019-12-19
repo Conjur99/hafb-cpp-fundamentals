5f0fa8a904d30be01fb1421eda56e87cc927ace4
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-19 16:27:24 -0700

    partial LA5-2 work

--- Module5/TO5-1/src/containers.cpp
+++ Module5/TO5-1/src/containers.cpp
@@ -30,3 +30,25 @@ void Display_by_State(std::map<std::string, int> &st_count)
 	}
     std::cout << "A total of "<< total << " records for all states" << std::endl;
 }
+
+void Load_Data(std::vector<Data> &data, const std::string &input_file)
+{
+    csvstream csvinput(input_file);
+
+    std::map<std::string, std::string> row;
+    Data temp;
+
+while (csvinput >> row)
+    {
+        // The key is the column name
+        // std::cout << row["id"] << "," << row["name"]    //TODO: unfinished
+        // << "," << row["animal"] << std::endl;
+        temp.id = row["id"];
+        temp.gender = row["gender"];
+        temp.school = row["school"];
+        temp.state = row["state"];
+        
+        data.push_back(temp);       //load Data struct into vector
+
+    }
+}
--- Module5/TO5-1/src/main.cpp
+++ Module5/TO5-1/src/main.cpp
@@ -5,6 +5,11 @@ using namespace std;
 // Main Function
 int main()
 {
+  std::vector<Data> info;
+  std:string infile = "../data/sample_data.csv";
+  Load_Data(info, infile);
+
+  cout << info.size << "records"  << std::endl;
 
   return 0;
 }
